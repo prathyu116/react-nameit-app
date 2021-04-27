ac3cc147022d2b608f002f6126dd7680f3903641
prathyu <prathyuprasad04669@gmail.com>
2021-04-28 02:14:24 +0530

    Updates

---
+++ precache-manifest.1a222126d5a42640474e453ba8744313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f34936def5e9f475c2326e4a76d717",
+    "url": "/github.io/react-nameit-app/index.html"
+  },
+  {
+    "revision": "c3e100da3eeb7a7456e3",
+    "url": "/github.io/react-nameit-app/static/css/main.e49412a2.chunk.css"
+  },
+  {
+    "revision": "5a067073eea3e9408acd",
+    "url": "/github.io/react-nameit-app/static/js/2.b5fece16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-nameit-app/static/js/2.b5fece16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e100da3eeb7a7456e3",
+    "url": "/github.io/react-nameit-app/static/js/main.e63d3606.chunk.js"
+  },
+  {
+    "revision": "3f1477521b10f8e4e7dc",
+    "url": "/github.io/react-nameit-app/static/js/runtime-main.154fdc09.js"
+  }
+]);
